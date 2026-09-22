3a1dd588f93cfef2c04e93f142e9c0e4e54b6a0c
Vixy <76610866+valerie-mc@users.noreply.github.com>
2026-09-22 14:45:09 -0700

    Added CMake folder

--- template/main.cpp
+++ template/main.cpp
@@ -4,71 +4,65 @@
 #else
 import vulkan_hpp;
 #endif
+
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <cstdlib>
 
-const uint32_t WIDTH  = 800;
-const uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication
-{
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
   public:
-	void run()
-	{
-		initWindow();
-		initVulkan();
-		mainLoop();
-		cleanup();
-	}
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
   private:
-	GLFWwindow *window = nullptr;
+    GLFWwindow *window = nullptr;
 
-	void initWindow()
-	{
-		glfwInit();
+    void initWindow() {
+        glfwInit();
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Disable resizing for now
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-	}
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
 
-	void initVulkan()
-	{
-	}
+    void initVulkan() {
 
-	void mainLoop()
-	{
-		while (!glfwWindowShouldClose(window))
-		{
-			glfwPollEvents();
-		}
-	}
+    }
 
-	void cleanup()
-	{
-		glfwDestroyWindow(window);
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
 
-		glfwTerminate();
-	}
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 };
 
-int main()
-{
-	try
-	{
-		HelloTriangleApplication app;
-		app.run();
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
+int main() {
+    try {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
