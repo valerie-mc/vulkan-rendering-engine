6e4ee40503eae29049d33e6310fa2543c7a239ba
Vixy <76610866+valerie-mc@users.noreply.github.com>
2026-09-22 17:00:17 -0700

    Work on setting up a Vulkan instance

--- l-run.sh
+++ l-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ~/Programming/C++/vulkan-rendering-engine/template
 cmake --build build
+./build/VulkanTutorial/VulkanTutorial
--- template/main.cpp
+++ template/main.cpp
@@ -28,6 +28,9 @@ class HelloTriangleApplication {
   private:
     GLFWwindow *window = nullptr;
 
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
+
     void initWindow() {
         glfwInit();
 
@@ -38,7 +41,39 @@ class HelloTriangleApplication {
     }
 
     void initVulkan() {
+        createInstance();
+    }
 
+    void createInstance() {
+        constexpr vk::ApplicationInfo appInfo {
+            .pApplicationName   = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+            .pEngineName        = "No Engine",
+            .engineVersion      = VK_MAKE_VERSION( 1, 0, 0 ),
+            .apiVersion         = vk::ApiVersion14
+        };
+
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+            if (std::ranges::none_of(
+                    extensionProperties,
+                    [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                    { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+        vk::InstanceCreateInfo createInfo {
+            .pApplicationInfo = &appInfo,
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions
+        };
     }
 
     void mainLoop() {
